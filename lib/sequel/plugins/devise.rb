643e86f50cfcc3be797a2a236a7ca874d610ebf7
Ekin Dursun <ekindursun@gmail.com>
2021-09-13 09:29:25 +0300

    Update deprecated name (#5)
    Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION has a new
    name, UNSAFE_ATTRIBUTES_FOR_SERIALIZATION, and the old name is deprecated.

--- lib/sequel/plugins/devise.rb
+++ lib/sequel/plugins/devise.rb
@@ -52,7 +52,7 @@ module Sequel
         end
 
         def devise_safe_keys
-          ::Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION
+          ::Devise::Models::Authenticatable::UNSAFE_ATTRIBUTES_FOR_SERIALIZATION
         end
       end
 
